2286de05a6197e7861496945a446566990b2e7dd
haxscramper <hax-scamper-git@protonmail.com>
2020-03-22 23:38:01 +0300

    [WIP][>>>]{HARGPARSE2}

--- hargparse2/src/hargparse2.nim
+++ hargparse2/src/hargparse2.nim
@@ -1,49 +1,366 @@
 import macros
-import helpers
+import strformat
+import result
+import hmisc/helpers
 import sequtils
 import strutils
 import options
 import deques
+import typeinfo
+import typetraits
+import tables
+
+template setOk[V, E](res: var Result[V, E], val: V): untyped =
+  ## Set ok value for result type and immediately return
+  res.ok val
+  return res
+
+template setErr[V, E](res: var Result[V, E], val: E): untyped =
+  ## Set error value for result type and immediately return
+  res.err val
+  return res
+
 
 type
-  OptDesc = object
-    name: string ## name of the command, used when acessing it in code
-    help: string ## Help string
-    pfix: seq[string] ## Possible prefixes to set given option (--opt)
+  OptDesc* = object
+    ## Data structure that will be used to generate command/options
+    ## tree. Configuration is first parsed into this structure and only
+    ## then used to generate tree.
+    name*: string ## name of the command, used when acessing it in code
+    help*: string ## Help string
+    docstr*: string ## Longer string, used to generate manpages or html
+                   ## documentation.
+    pfix*: seq[string] ## Possible prefixes to set given option (--opt)
 
-    maxValues: int ## Maximum number of times option can be set. If
+    maxValues*: int ## Maximum number of times option can be set. If
                    ## this equals zero option type will be `bool`. One
                    ## for `T`. Two or more will create `seq[T]`.
 
-
-    exclRules: Option[NimNode] ## Boolean expression to check whether
+    exclRules*: Option[NimNode] ## Boolean expression to check whether
                                ## or not it is possible to use command
                                ## (is it mutally exclisive with some
                                ## other command or not)
 
-    parsecheck: Option[NimNode] ## Code that will be evaluated to
+    parsecheck*: Option[NimNode] ## Code that will be evaluated to
                                 ## check validity of the argument.
 
-    parseto: Option[typedesc] ## Type that *single* use of the option
+    parseto*: Option[NimNode] ## Type that *single* use of the option
                               ## can be parsed to. This is `T` in
                               ## `maxValues`
 
-  ArgDesc = object
-    name: string ## name of the command, used when acessing it in code
-    help: string ## Help string
-    parsecheck: Option[NimNode]
-    parseto: Option[typedesc]
+  ArgDesc* = object
+    ## Description of the command argument.
+    name*: string ## name of the command, used when acessing it in code
+    variadic*: bool ## Whether or not argument might accept unlimited
+                   ## number of arguments. Variadic options **must**
+                   ## be placed last.
+    help*: string ## Help string, displayed when `--help` is invoked.
+    docstr*: string ## Longer string, used to generate manpages or html
+                   ## documentation.
+    parsecheck*: Option[NimNode] ## Code that will be evaluated to
+                                ## check validty of the argument.
+    parseto*: Option[NimNode] ## Type that *single* use of the
+                          ## argument can be parsed into. If
+                          ## argument is `variadic` than each
+                          ## entry will be checked.
 
-  CommandDesr = object
-    name: string
-    help: string
-    args: seq[ArgDesc]
-    opts: seq[OptDesc]
+  CommandDescr* = object
+    # ## Description of the utility command
+    name*: string ## Name of the command
+    help*: string ## Help message to be displayed in help page
+    dosctr*: string ## Longer string, used to generate manpages or html
+                   ## documentation.
+    args*: seq[ArgDesc] ## Arguments for the command. Arguments are
+                       ## tried after subcommands - in case of
+                       ## non-disjoint sets of values subcommand
+                       ## always takes precedence.
+    subs*: seq[CommandDescr] ## Subcommands.
+    opts*: seq[OptDesc] ## Option related to the command.
 
-# The same is for `arg`
-macro opt(name: static[string]): typed =
-  ## Replace with call to correct getter function with type signature
-  ## corresponding to `parseto` field in `OptDesc`
-  quote do:
-    echo "test"
+var descriptionsComptime {.compileTime.}: Table[string, CommandDescr]
 
+type
+  ParseErrorKind* = enum
+    ## Kind of command parse error
+    pekUnknownOption
+    pekInvalidValue
+    pekTooMuchValues
+    pekMissingArgument
+    pekTokenParseError
+
+  ParseError* = object
+    ## Command parse error
+    message*: string
+    kind*: ParseErrorKind
+
+type
+  CommandTokenKind* = enum
+    ## Type of command line token
+    ctkLongFlag
+    ctkShortFlag
+
+    ctkLongOption
+    ctkShortOption
+
+    ctkArgument
+    ctkCommand
+
+  PossibleParse* = seq[CommandTokenKind]
+    ## Possible parses for ambigout command line options (i.e.
+    ## `-hello` might be a mistyped long flag, short flag with option
+    ## or multiple short flags with last one havin argumnet (`-h`
+    ## `-e=llo` is a possibilty))
+
+  ParsedToken* = object
+    ## Single, unambigously parsed token entry
+    kind: CommandTokenKind
+    key: string
+    value: string
+
+func classifyToken(tok: string): PossibleParse =
+  discard
+
+func isUnambiguous(optParse: PossibleParse): bool =
+  optParse.len == 1
+
+func parseToken(tok: string, kind: CommandTokenKind
+               ): Result[ParsedToken, ParseError] =
+  ## Parse single unambigous token
+  var res = ParsedToken(kind: kind)
+  case kind:
+    of ctkArgument: res.value = tok
+    of ctkLongFlag:
+      if tok.startsWith("--"):
+        res.key = tok[2..^1]
+        result.ok res
+      else:
+        result.err ParseError(
+          kind: pekTokenParseError,
+          message: &"Cannot parse '{tok}' as long flag, missing '--'"
+        )
+    of ctkShortFlag:
+      if tok.startsWith("-") and tok.len == 2:
+        res.key = tok[1..^1]
+        result.ok res
+      else:
+        result.err ParseError(
+          kind: pekTokenParseError,
+          message: &"Cannot parse '{tok}' as short flag")
+    else:
+      discard
+
+when isMainModule:
+  assert parseToken("--hello", ctkLongFlag).isOk()
+  assert parseToken("-Hello", ctkLongFlag).isErr()
+
+func separateTokens(
+  tok: string, comm: CommandDescr
+     ): Result[seq[ParsedToken], ParseError] =
+  ## Separate token into set of parsed tokens according to
+  ## specification.
+  let classification = tok.classifyToken()
+  if classification.isUnambiguous():
+    let res = tok.parseToken(classification[0])
+    if res.isOk():
+      result.ok @[res.get]
+    else:
+      result.err res.error
+  else:
+    if tok.startsWith("-"):
+      discard
+
+func toNimIdentName(name: string): string =
+  ## Convert name of the command/option/argument to string that might
+  ## be used as nim identifier name (remote dashes, whitespaces and
+  ## any other characters not allowed in nim variable names)
+  name.replace("-", "_")
+
+func makeNimType(name: string, flds: seq[NimNode]): NimNode =
+  ## Create new nim object type
+  nnkStmtList.newTree(
+    nnkTypeSection.newTree(
+      nnkTypeDef.newTree(
+        newIdentNode(name),
+        newEmptyNode(),
+        nnkObjectTy.newTree(
+          newEmptyNode(),
+          newEmptyNode(),
+          nnkRecList.newTree(flds)))))
+
+func joinTypeNames*(names: seq[string], comm: CommandDescr): string =
+  ## Join type names to use in generated types
+  (names & @[comm.name]).map(toNimIdentName).map(capitalizeAscii).join("")
+
+func makeOptTypeName(names: seq[string], comm: CommandDescr): string =
+  ## Get name of the option type for given list of parent command
+  ## `names`
+  "opts" & joinTypeNames(names, comm)
+
+func makeCommandTypeName(names: seq[string], comm: CommandDescr): string =
+  ## Get name of the command type for given list of parent command
+  ## `names` (should include final command)
+  "command" & joinTypeNames(names, comm)
+
+func makeCommandParserName(names: seq[string], comm: CommandDescr): string =
+  ## Get name of the command type for given list of parent command
+  ## `names` (should include final command)
+  "parse" & joinTypeNames(names, comm)
+
+func makeCommandSetterName(names: seq[string], comm: CommandDescr): string =
+  ## Get command value setter proc name
+  "setvalFor" & joinTypeNames(names, comm)
+
+func makeCommandOptionsType(comm: CommandDescr, parentComms: seq[string]): NimNode =
+  ## Generate type describind possible options for the command
+  ## description. This is what CLI options will be parsed **into**.
+  let name = makeOptTypeName(parentComms, comm)
+  let flds = comm.opts.mapIt(
+    block:
+      nnkIdentDefs.newTree(
+        newIdentNode(it.name.toNimIdentName()),
+        it.parseto.get(newIdentNode("string")),
+        newEmptyNode()
+      )
+  )
+  result = makeNimType(name, flds)
+
+func makeCommandType(comm: CommandDescr, parentComms: seq[string]): NimNode =
+  ## Generate type describing cli command
+  let name = makeCommandTypeName(parentComms, comm)
+  let optName = makeOptTypeName(parentComms, comm)
+  result = makeNimType(name, @[
+    nnkIdentDefs.newTree(
+      ident("options"),
+      ident(optName),
+      newEmptyNode()
+    )
+  ])
+
+func makeCommandSetter(comm: CommandDescr, parentComms: seq[string]): NimNode =
+  ## Create procedure for settings arguments to the command. The
+  ## procedure modifies value of the command (`comm` argument)
+  ## in-place. Second argument is single, unambigously parsed token
+  ## entry. Generated procedure performs necessary checks and might
+  ## generate error in parsing. Error returned as `err` value for
+  ## `Result`.
+  let name = makeCommandSetterName(parentComms, comm).ident()
+  let comm = makeCommandTypeName(parentComms, comm).ident()
+  result = quote do:
+    proc `name`(comm: var `comm`, tok: ParsedToken): Result[bool, ParseError] =
+      discard
+
+
+func makeCommandParser(comm: CommandDescr, parentComms: seq[string]): NimNode =
+  ## Generate function for parsing input string to command
+  let commResType = newIdentNode(makeCommandTypeName(parentComms, comm))
+  let parserName = newIdentNode(makeCommandParserName(parentComms, comm))
+  let setterName = newIdentNode(makeCommandSetterName(parentComms, comm))
+  let commName = makeCommandTypeName(parentComms, comm)
+  let commLit = newStrLitNode(commName)
+
+  result = quote do:
+    proc `parserName`(tokens: seq[string]): Result[`commResType`, ParseError] =
+      # TODO get description without raising side-effects
+      let descr = descriptions[`commLit`]
+      var res: `commResType`
+      for tok in tokens:
+        let separate = tok.separateTokens(descr)
+        if separate.isOk():
+          for stok in separate.get():
+            let setres = `setterName`(res, stok)
+            if setres.isErr():
+              result.setErr setres.error
+        else:
+          result.setErr separate.error
+
+
+
+
+proc makeCommandDeclaration(comm: CommandDescr, parentComm: seq[string]): NimNode =
+  ## Generate types for command `comm`
+  let optType = makeCommandOptionsType(comm, parentComm)
+  let commType = makeCommandType(comm, parentComm)
+  let parser = makeCommandParser(comm, parentComm)
+  let setter = makeCommandSetter(comm, parentComm)
+  descriptionsComptime[makeCommandTypeName(parentComm, comm)] = comm
+  result = quote do:
+    `optType`
+    `commType`
+    `setter`
+    `parser`
+
+macro test() =
+  let commTest = CommandDescr(
+    opts: @[OptDesc(
+      name: "test",
+      parseto: some(quote do: int)
+    )],
+    name: "77",
+  )
+
+  let res = makeCommandDeclaration(commTest, @[])
+  # echo res.toStrLit()
+  result = res
+
+func initCodegen*(val:
+    char   | int     | int8   | int16 |
+    int32  | int64   | uint   | uint8 |
+    uint16 | uint32  | uint64 | bool  |
+    string | float32 | float64
+  ): NimNode =
+    debugEcho "primitive ", typeof(val)
+    newLit(val)
+
+func initCodegen*[T](arr: seq[T]): NimNode =
+  debugEcho: "sequence"
+  nnkStmtList.newTree(
+    nnkPrefix.newTree(
+      newIdentNode("@"),
+      nnkBracket.newTree(arr.mapIt(
+        block:
+          debugEcho: "List item"
+          it.initCodegen()
+        ))))
+
+func initCodegen*[T: tuple | object](obj: T): NimNode =
+  debugEcho "tuple"
+  var fieldInit: seq[NimNode]
+  for name, value in obj.fieldPairs:
+    debugEcho: "sdffdsf"
+    when not isNamedTuple(typeof T):
+      fieldInit.add initCodegen(value)
+    else:
+      fieldInit.add nnkExprColonExpr.newTree(ident(name), initCodegen(value))
+
+  nnkStmtList.newTree(nnkPar.newTree(fieldInit))
+
+func initCodegen*[K, V](tbl: Table[K, V]): NimNode =
+  debugEcho "table"
+  var fieldInit: seq[NimNode]
+  for key, val in tbl:
+    fieldInit.add nnkPar.newTree(
+      initCodegen[typeof key](key), initCodegen[typeof val](val)
+    )
+
+  # makeDotExpr(fieldInit.makeBracket, ident("toTable")).makeCall
+  nnkStmtList.newTree(
+    nnkCall.newTree(
+      nnkBracketExpr.newTree(
+        nnkDotExpr.newTree(
+          nnkBracket.newTree(fieldInit),
+          newIdentNode("toTable")
+        ),
+        newIdentNode($typeof(K)),
+        newIdentNode($typeof(V)))))
+
+macro generate(): untyped =
+  result = initCodegen(descriptionsComptime)
+  echo result.toStrLit()
+
+var descriptions {.compiletime.} = descriptionsComptime
+
+test()
+
+discard generate()
+
+# let parsed = parse77(@["--test:12"])
