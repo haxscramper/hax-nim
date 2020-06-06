850853a1f9e0f900bc2543e9471a67523ea17d39
haxscramper <hax-scamper-git@protonmail.com>
2020-06-06 15:58:22 +0300

    [FEATURE]{HTERMS} Trie data structure implementation
    Added:
      - Test implementation for trie data structure

--- hmisc/src/test1.nim
+++ hmisc/src/test1.nim
@@ -30,25 +30,65 @@ type
   Tree = object
     val: string
     sub: seq[Tree]
+when false:
+  import deques
+  import strutils
 
-import deques
-import strutils
+  iterator nodes(tree: Tree): tuple[node: Tree, path: seq[int]] =
+    var que: Deque[(Tree, seq[int])]
+    que.addLast((tree, @[0]))
+    while que.len > 0:
+      let (nowTerm, path) = que.popFirst()
+      for idx, subTerm in nowTerm.sub:
+        que.addLast((subTerm, path & @[idx]))
 
-iterator nodes(tree: Tree): tuple[node: Tree, path: seq[int]] =
-  var que: Deque[(Tree, seq[int])]
-  que.addLast((tree, @[0]))
-  while que.len > 0:
-    let (nowTerm, path) = que.popFirst()
-    for idx, subTerm in nowTerm.sub:
-      que.addLast((subTerm, path & @[idx]))
+      yield (node: nowTerm, path: path)
 
-    yield (node: nowTerm, path: path)
+  let test = Tree(val: "12", sub: @[
+    Tree(val: "222", sub: @[Tree(val: "(())")]),
+    Tree(val: "222", sub: @[Tree(val: "(()00)")]),
+    Tree(val: "222", sub: @[Tree(val: "((--))")])
+  ])
 
-let test = Tree(val: "12", sub: @[
-  Tree(val: "222", sub: @[Tree(val: "(())")]),
-  Tree(val: "222", sub: @[Tree(val: "(()00)")]),
-  Tree(val: "222", sub: @[Tree(val: "((--))")])
-])
+  for (node, path) in test.nodes():
+    echo path.join(" "), " ", node.val
 
-for (node, path) in test.nodes():
-  echo path.join(" "), " ", node.val
+import tables, options
+
+type
+  Trie*[Key, Val] = object
+    subn: Table[Key, Trie[Key, Val]]
+    value: Option[Val]
+
+proc `[]`*[Key, Val](trie: Trie[Key, Val], path: openarray[Key]): Val =
+  ## Get value at path
+  var curr = trie
+  for key in path:
+    if key in curr.subn:
+      curr = curr.subn[key]
+    else:
+      raise newException(
+        KeyError, "Trie key not found: " & $path)
+
+  if curr.value.isNone():
+    raise newException(
+      KeyError, "Trie key not found: " & $path)
+  else:
+    curr.value.get()
+
+proc `[]=`*[Key, Val](
+  trie: var Trie[Key, Val], path: openarray[Key], val: Val) =
+  ## Set value at path
+  var curr: ptr Trie[Key, Val] = addr trie
+  for key in path:
+    if key notin curr.subn:
+      curr.subn[key] = Trie[Key, Val]()
+
+    curr = addr curr.subn[key]
+
+  curr.value = some(val)
+
+var tr = Trie[int, string]()
+
+tr[[0, 2]] = "12"
+assert tr[[0, 2]] == "12"
