db0df09f2ea83be521534eec730de008c586aa91
haxscramper <hax-scamper-git@protonmail.com>
2020-06-10 21:14:43 +0300

    [WIP][FEATURE]{HPARSE} Topological sort
    Changed:
      - Fixed compilation errors for grammar instantiation
    Added:
      - WIP topological sort implementation

--- hparse/src/grammars.nim
+++ hparse/src/grammars.nim
@@ -1,4 +1,5 @@
-import tables
+import tables, sugar, sequtils
+export tables
 
 type
   NTermSym* = string
@@ -24,12 +25,12 @@ type
       of pkAlternative, pkConcat:
         patts*: seq[Patt[TKind]]
       of pkOptional, pkZeroOrMore, pkOneOrMore:
-        opt*: Patt ## Single instance that will be repeated [0..1],
-        ## [0..n] or [1..n] times respectively
+        opt*: Patt[TKind] ## Single instance that will be repeated
+        ## [0..1], [0..n] or [1..n] times respectively
 
   Rule*[TKind] = object
-    nterm: NTermSym
-    patts: Patt[TKind]
+    nterm*: NTermSym
+    patts*: Patt[TKind]
 
   Grammar*[TKind] = object
     rules*: seq[Rule[TKind]]
@@ -37,16 +38,65 @@ type
 type
   FirstSet*[TKind] = set[TKind]
   NTermSets*[TKind] = object
-    first: Table[NTermSym, FirstSet[TKind]]
+    first*: Table[NTermSym, FirstSet[TKind]]
 
-  CompPatt*[Tok] = object
-    patt: Patt
-    first: FirstSet
+  CompPatt*[TKind] = object
+    patt*: Patt[TKind]
+    first*: FirstSet[TKind]
 
 
   CompRule*[TKind] = object
-    nterm: NTermSym
-    patts: CompPatt[TKind]
+    nterm*: NTermSym
+    patts*: CompPatt[TKind]
 
   CompGrammar*[TKind] = object
     rules*: seq[CompRule[TKind]]
+
+proc nthType1*[T1, T2](a: (T1, T2)): T1 = discard
+proc nthType2*[T1, T2](a: (T1, T2)): T2 = discard
+
+# iterator pairs*[T1, T2](s: openarray[(T1, T2)]): (T1, T2) =
+#   for item in s:
+#     yield item
+
+template mapPairs*(s: untyped, op: untyped): untyped =
+  const openarrPairs = (s is array) or (s is seq) or (s is openarray)
+  when openarrPairs:
+    type TLhs = type((s[0][0]))
+    type TRhs = type((s[0][1]))
+    static:
+      echo "using array types"
+  else:
+    type TLhs = type((pairs(s).nthType1))
+    type TRhs = type((pairs(s).nthType2))
+    static:
+      echo "using `pairs()` types"
+
+  static:
+    echo typeof(TLhs)
+    echo typeof(TRhs)
+
+  type TRes = type((
+    block:
+      var lhs {.inject.}: TLhs
+      var rhs {.inject.}: TRhs
+      op))
+
+  var res: seq[TRes]
+
+  when openarrPairs:
+    for (lhsTmp, rhsTmp) in s:
+      let lhs {.inject.} = lhsTmp
+      let rhs {.inject.} = rhsTmp
+      res.add op
+  else:
+    for lhsTmp, rhsTmp in s:
+      let lhs {.inject.} = lhsTmp
+      let rhs {.inject.} = rhsTmp
+      res.add op
+
+  res
+
+proc toGrammar*[TKind](
+  table: openarray[(string, Patt[TKind])]): Grammar[TKind] =
+  result.rules = table.mapPairs(Rule[TKind](nterm: lhs, patts: rhs))
--- hparse/src/test.nim
+++ hparse/src/test.nim
@@ -4,35 +4,137 @@ import options
 type
   Parser[Val, Buf] = proc(buf: Buf): Val
 
-proc parseStr(str: string): Parser[string, char] = discard
-var parser: Parser[string, char]
+when false:
+  proc parseStr(str: string): Parser[string, char] = discard
+  var parser: Parser[string, char]
 
 
-proc genericImpl[T](arg: T) =
-  echo "using generic"
+  proc genericImpl[T](arg: T) =
+    echo "using generic"
 
-proc genericImpl[T](arg: seq[T]) =
-  echo "Using less generic"
+  proc genericImpl[T](arg: seq[T]) =
+    echo "Using less generic"
 
-proc genericImpl(arg: string) =
-  echo "using speciialiation"
+  proc genericImpl(arg: string) =
+    echo "using speciialiation"
 
-genericImpl("hello")
-genericImpl(@["Hello"])
-genericImpl(12)
+  genericImpl("hello")
+  genericImpl(@["Hello"])
+  genericImpl(12)
 
-# macro makeBufType(t: untyped): untyped =
-#   echo t.toStrLit()
-#   # let res =
-#   #   if t is string:
-#   #     typeof(string)
-#   #   else:
-#   #     typeof(seq[t])
+  # macro makeBufType(t: untyped): untyped =
+  #   echo t.toStrLit()
+  #   # let res =
+  #   #   if t is string:
+  #   #     typeof(string)
+  #   #   else:
+  #   #     typeof(seq[t])
 
-# echo typeof(makeBufType(string))
+  # echo typeof(makeBufType(string))
 
-import options
+  import options
 
-proc t[T](arg: Option[tuple[field1: T, field2: int]]) = discard
+  proc t[T](arg: Option[tuple[field1: T, field2: int]]) = discard
 
-t(some((1, 2)))
+  t(some((1, 2)))
+
+when false:
+  iterator pairs(a: int): (int, int) = discard
+  iterator items(a: int): int = discard
+
+  proc nthType1[T1, T2](a: (T1, T2)): T1 = discard
+  proc nthType2[T1, T2](a: (T1, T2)): T2 = discard
+
+  type T1 = type((pairs(12).nthType1))
+  type T2 = type((items(2)))
+  type T3 = type(((12, "float")[1]))
+
+
+  iterator pairs*[T1, T2](s: openarray[(T1, T2)]): (T1, T2) =
+    for item in s:
+      yield item
+
+  echo typeof(pairs([(1, "12")]).nthType2)
+
+  let inp = {12 : "float"}
+
+  echo (inp is array) or (inp is seq)
+
+  # echo typeof(inp[0][0])
+  # echo typeof(inp[0][1])
+
+
+when false:
+
+  func assrt() = assert false
+
+  assrt()
+
+  func test() = raise newException(IOError, "12")
+
+  test()
+
+#=================================  v1  ==================================#
+if false:
+  proc test(pr: static proc(p: int): string = nil) =
+    when pr == nil:
+      let impl = proc(p: int): string = "default"
+    else:
+      let impl = pr
+
+    echo "res: ", impl(12)
+
+  test(proc(b: int): string = "hhh")
+
+
+#=================================  v2  ==================================#
+import hashes
+
+if false:
+  proc test(pr: static proc(p: int): Hash = nil) =
+    when pr == nil:
+      let impl = proc(p: int): Hash = discard
+    else:
+      let impl = pr
+
+    echo "res: ", impl(12)
+
+  test(proc(b: int): Hash = discard)
+
+
+#=================================  v3  ==================================#
+import tables, sets
+if true:
+  proc test(
+    vt: openarray[string],
+    dp: proc(v: string): seq[Hash],
+    pr: static proc(p: string): Hash =
+                   (proc(p: string): Hash = hash(p))) =
+
+    var t: Table[Hash, HashSet[Hash]]
+
+  test(
+    vt = ["12"],
+    dp = proc(v: string): seq[Hash] = @[hash(v)]
+  )
+
+#=================================  v4  ==================================#
+
+if true:
+  proc test[T](
+    vt: openarray[T],
+    dp: proc(v: T): seq[Hash],
+    pr: proc(p: T): Hash = (proc(p: T): Hash = hash(p))) =
+
+    var t: Table[Hash, HashSet[Hash]]
+
+  test(
+    ["12"],
+    proc(v: string): seq[Hash] = @[hash(v)],
+    proc(b: string): Hash = hash(b)
+  )
+
+  test(
+    ["12"],
+    proc(v: string): seq[Hash] = @[hash(v)]
+  )
