5cb91be3c0d3ace487365878a084e9fcc3de25f9
haxscramper <haxscramper@gmail.com>
2020-07-11 16:19:35 +0300

    [FEATURE] Geometric primitives

--- hmisc/src/hmisc/types/hprimitives.nim
+++ hmisc/src/hmisc/types/hprimitives.nim
@@ -1,50 +1,5 @@
 import unicode
 
-#=========================================================================#
-#=======================  Geometrical primitives  ========================#
-#=========================================================================#
-
-#================================  Size  =================================#
-
-type
-  Size* = object
-    width: int
-    height: int
-
-
-func width*(size: Size): int = size.width
-func height*(size: Size): int = size.height
-func makeSize*(w, h: int): Size = Size(width: w, height: h)
-
-#================================  Point  ================================#
-
-type
-  Point*[Num] = object
-    x: Num
-    y: Num
-
-func makePoint*[Num](x, y: Num): auto = Point[Num](x: x, y: y)
-func makePoint*[Num](pos: (Num, Num)): auto = Point[Num](x: pos[0], y: pos[1])
-func shiftX*[Num](p: Point[Num], dx: Num): Point[Num] =
-  makePoint[Num](p.x + dx, p.y)
-
-func shiftY*[Num](p: Point[Num], dy: Num): Point[Num] =
-  makePoint[Num](p.x, p.y + dy)
-
-
-func shiftXY*[Num](p: Point[Num], dx: Num, dy: Num): Point[Num] =
-  makePoint[Num](p.x + dx, p.y + dy)
-
-func shiftXY*[Num](p: (Num, Num), dx: Num, dy: Num): (Num, Num) =
-  (p[0] + dx, p[1] + dy)
-
-#===============================  radian  ================================#
-
-type
-  Radian* = distinct float
-
-const whitespaceRune*: Rune = toRunes(" ")[0]
-var emptyRune*: Rune
 
 #=========================================================================#
 #===========================  String helpers  ============================#
